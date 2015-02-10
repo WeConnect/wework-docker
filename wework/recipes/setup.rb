42a7cd45771de577b117b7b50c347a8760bfad74
Tom Clark <whitespace@gmail.com>
2015-02-10 17:16:32 -0500

    Uses templates to generate dockercfg file

--- wework/recipes/setup.rb
+++ wework/recipes/setup.rb
@@ -1,4 +1,6 @@
-remote_file File.expand_path("~/.dockercfg") do
-  source "https://s3.amazonaws.com/spaceman-docker/.dockercfg"
-  action :create_if_missing
+template "/root/.dockercfg" do
+  source "dockercfg.erb"
+  mode '0600'
+  owner 'root'
+  group 'root'
 end
