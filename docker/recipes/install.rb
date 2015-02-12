713db5a7c9ba811f92735a5bd34f47880b50d1a9
Tom Clark <whitespace@gmail.com>
2015-02-11 19:21:43 -0500

    Fixes missing directories so docker pull doesn't bail

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -6,6 +6,21 @@ package "docker.io" do
   action :install
 end
 
+directory '/var/lib/docker/devicemapper/devicemapper/data' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/var/lib/docker/devicemapper/devicemapper/metadata' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
 service "docker" do
   action :start
 end
